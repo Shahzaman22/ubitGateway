0fa9e3be796afe3e7970ccff0f116df649ae5914
Muhammad Usama <muhammad.usama@technyxsystems.com>
2023-05-17 10:42:47 +0500

    message setup testing

--- Apps/controllers/chat.controller.js
+++ Apps/controllers/chat.controller.js
@@ -4,42 +4,48 @@
 // const server = http.createServer(app);
 // const { Server } = require("socket.io");
 // const io = new Server(server);
-const socketIo = require("socket.io");
-const cors = require("cors")
-const Chat = require('../model/chat')
+// const socketIo = require("socket.io");
+const cors = require("cors");
+const Chat = require("../model/chat");
 // const io = require('../../index')
-const {myServer} = require('../../index')
+const { myServer } = require("../../index");
+const io = require("../../index");
 
-const io = socketIo(myServer, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
+// const io = socketIo(myServer, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
 
-exports.get =  async(req, res) => {
-     await res.sendFile(__dirname + '/Frontend/index.html');
-     
-  };
+exports.get = async (req, res) => {
+  await res.sendFile(__dirname + "/Frontend/index.html");
+};
 
 exports.getExecutive = async (req, res) => {
-   await res.sendFile(__dirname + '/Frontend/executive.html');
-  }; 
-  
-exports.getEngineer =  async (req, res) => {
-   await res.sendFile(__dirname + '/Frontend/engineer.html');
-  };   
-  
+  await res.sendFile(__dirname + "/Frontend/executive.html");
+};
 
-  
-  
-
-
-  io.on("connection", (socket) => {
-    console.log("a user connecteddddddd");
-    socket.on('message', (message) => {
-      console.log(`Received message from client ${socket.id}:`, message)});
+exports.getEngineer = async (req, res) => {
+  await res.sendFile(__dirname + "/Frontend/engineer.html");
+};
+// TEST CONTROLLER TO SAVE MESSAGE TO DB
+exports.testSavingtoDB = async (data) => {
+  console.log(data);
+  const chat = new Chat({
+    user: data.userId,
+    message: data.message,
   });
-// const adminNameSpace = io.of('/admin');  
+  const aa = await chat.save();
+  console.log("success");
+};
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("message", (message) => {
+//     console.log(`Received message from client ${socket.id}:`, message);
+//   });
+// });
+// const adminNameSpace = io.of('/admin');
 //   adminNameSpace.on('connect', (socket) => {
 //     console.log('a user connected');
 //     socket.on('join', (data) => {
@@ -58,7 +64,6 @@ exports.getEngineer =  async (req, res) => {
 //         await chat.save();
 //         adminNameSpace.in(data.room).emit('chat message', data.msg);
 //       });
-      
 
 //       socket.on('send message to all',async (data) => {
 //         console.log('message: ' + data.msg);
@@ -70,6 +75,3 @@ exports.getEngineer =  async (req, res) => {
 //         adminNameSpace.emit('chat message', data.msg);
 //       });
 //   });
-
-
-  
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const express = require("express");
 const http = require("http");
-// const socketIo = require("socket.io");
+const socketIo = require("socket.io");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -9,14 +9,37 @@ const app = express();
 
 const server = http.createServer(app);
 
-// const io = socketIo(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//   },
-// }); //in case server and client run on different urls
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+}); //in case server and client run on different urls
 const session = require("express-session");
 const path = require("path");
+const { testSavingtoDB } = require("./Apps/controllers/chat.controller");
 const port = process.env.PORT || 4000;
+
+// Handle incoming socket connections only on the chat route
+io.of("/chat").on("connection", (socket) => {
+  // GETTING SOCKET ID
+  console.log("User connected:", socket.id);
+  // WE CAN HAVE USERID IN OBJECT FROM FRONTEND
+  socket.on("message", (data) => {
+    // console.log("Received message:", message);
+    testSavingtoDB(data);
+
+    // Handle the message here and send a response if needed
+  });
+  // Handle incoming chat messages
+  // socket.on("send_message", (data) => {
+  //   console.log("Received message:", data);
+  //   // Broadcast the message to all connected clients
+  //   // io.of('/chat').emit('receive_message', {
+  //   //   user: data.user,
+  //   //   text: data.text
+  //   // });
+  // });
+});
 
 app.use(express.json());
 app.use(
@@ -54,6 +77,6 @@ app.use("/api", require("./Apps/routes/app.routes"));
 
 //PORT
 const myServer = server.listen(port, console.log(`Connecte to port ${port}`));
-
+module.exports = io;
 // module.exports = io;
-module.exports = {myServer};
+module.exports = { myServer };
