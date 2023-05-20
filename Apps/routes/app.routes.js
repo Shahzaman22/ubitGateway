2608a7df85b95a36e1903b0b8e6656448b36f1ec
[Shahzaman] <shahzaman.aftab@gmail.com>
2023-05-20 14:35:23 +0500

    Removed Chat-app

--- Apps/routes/app.routes.js
+++ Apps/routes/app.routes.js
@@ -3,7 +3,6 @@ let router = express.Router();
 
 router.use('/users',require('./user.routes'))
 router.use('/auth', require('./auth.routes'))
-router.use('/chat', require('./chat.routes'))
 
 
 module.exports = router;
